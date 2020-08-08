9855817a81a8420996b0c7742c0dd148f73266a1
Anthony Cruz <anthonyleierlw@gmail.com>
2020-08-08 14:43:00 -0300

    Validações de Formulário

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -57,6 +57,16 @@ export default class Auth extends Component {
 	};
 
 	render() {
+		const validations = [];
+		validations.push(this.state.email && this.state.email.includes('@'));
+		validations.push(this.state.password && this.state.password.length >= 6);
+		if (this.state.stageNew) {
+			validations.push(this.state.name && this.state.name.trim().length >= 3);
+			validations.push(this.state.password == this.state.confirmPassword);
+		}
+
+		const validForm = validations.reduce((total, current) => total && current);
+
 		return (
 			<ImageBackground source={backgroundImage} style={styles.background}>
 				<Text style={styles.title}>Tasks</Text>
@@ -96,8 +106,8 @@ export default class Auth extends Component {
 							secureTextEntry={true}
 						/>
 					) : null}
-					<TouchableOpacity onPress={this.siginOrSignup}>
-						<View style={styles.button}>
+					<TouchableOpacity onPress={this.siginOrSignup} disabled={!validForm}>
+						<View style={[styles.button, validForm ? {} : {backgroundColor: '#AAA'}]}>
 							<Text style={styles.buttonText}>{this.state.stageNew ? 'Registrar' : 'Entrar'}</Text>
 						</View>
 					</TouchableOpacity>
