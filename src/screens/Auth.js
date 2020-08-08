1016b97a94b614024f7b0fe092f28945bde5adcd
Anthony Cruz <anthonyleierlw@gmail.com>
2020-08-08 14:17:10 -0300

    Integração de Registro com Backend

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,24 +1,44 @@
 import React, {Component} from 'react';
 import {ImageBackground, Text, StyleSheet, View, TouchableOpacity, Alert} from 'react-native';
+import axios from 'axios';
 
 import AuthInput from '../components/AuthInput';
 import commonStyles from '../commonStyles';
 import backgroundImage from '../../assets/imgs/login.jpg';
 
+import {server, showError, showSuccess} from '../common';
+
+const initialState = {
+	name: '',
+	email: '',
+	password: '',
+	confirmPassword: '',
+	stageNew: false,
+};
+
 export default class Auth extends Component {
-	state = {
-		name: '',
-		email: '',
-		password: '',
-		confirmPassword: '',
-		stageNew: false,
-	};
+	state = {...initialState};
 
 	siginOrSignup = () => {
 		if (this.state.stageNew) {
-			Alert.alert('Sucesso!', 'Criar conta');
+			this.signup();
 		} else {
 			Alert.alert('Sucesso!', 'Login');
+		}
+	};
+
+	signup = async () => {
+		try {
+			await axios.post(`${server}/signup`, {
+				name: this.state.name,
+				email: this.state.email,
+				password: this.state.password,
+				confirmPassword: this.state.confirmPassword,
+			});
+			showSuccess('Usuário cadastrado');
+			this.setState(...initialState);
+		} catch (e) {
+			showError(e);
 		}
 	};
 
